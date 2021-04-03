d3307dd8a48ffadf2f7e9a08fe2b1bc56ca86455
ibrahimsaddam <ibrahimcse.eng@gmail.com>
2021-04-03 15:46:50 +0600

    passing Function as props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import BlogHomePageCard from './BlogCard';
+import { isArrayEmpty as isMyArrayEmpty } from './Utils';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    showBlogs: true,
+
+    blogArr: [
+    {
+      id: 1,
+      title: 'Blog Title 1',
+      description: 'John Wick John WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn Wick',
+      likeCount: 0,
+    },
+    {
+      id: 2,
+      title: 'Blog Title 2',
+      description: 'John Wick John WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn Wick',
+      likeCount: 0,
+    },
+    {
+      id: 3,
+      title: 'Blog Title 3',
+      description: 'John Wick John WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn WickJohn Wick',
+      likeCount: 0,
+    }
+  ]
+}
+ 
+  onLikeBtnClick = (pos) => {
+    const updatedBlogList = this.state.blogArr;
+    const updatedBlogObj = updatedBlogList[pos];
+
+    updatedBlogObj.likeCount = updatedBlogObj.likeCount +1;
+    updatedBlogList[pos] = updatedBlogObj;
+
+    this.setState({blogArr: updatedBlogList});
+
+    // console.log(updatedBlogObj);
+  }
+
+
+  
+
+  onHideBtnClick = () => {
+    // let updatedState = !this.state.showBlogs;
+    this.setState((prevState, prevProps) => {
+      return { showBlogs: !prevState.showBlogs }
+    });
+
+    console.log(this.state.showBlogs);
+  }
+
+  render() {
+    console.log('Render Called');
+    console.log(this.state);
+
+    const blogCards = isMyArrayEmpty(this.state.blogArr) ? [] : this.state.blogArr.map((item, pos) => {
+
+      return (
+        <BlogHomePageCard className={'Blog'} key={pos} title={item.title}
+          description={item.description} likeCount={item.likeCount} id={item.id}
+          onLikeBtnClick={() => this.onLikeBtnClick(pos)} />
+        // <div className="BlogCard" key={item.id}>
+        //   <h3>{item.title}</h3>
+        //   <p>{item.description}</p>
+        // </div>
+      )
+    })
+
+    return(
+      <div className="App">
+        <button onClick={this.onHideBtnClick}>{this.state.showBlogs ? 'Hide List' : 'Show List'}</button>
+        <br></br>
+        { this.state.showBlogs ? blogCards: null  }
+      </div>
+    )
+  }
+
 }
 
 export default App;
